8645cca9fee35e005a290ec0455b1d5eda87aaf0
junhyuk <dejavukk1@gmail.com>
2020-05-10 22:48:19 +0900

    TextField추가, Picker추가.

--- SwiftUIDemo/ContentView.swift
+++ SwiftUIDemo/ContentView.swift
@@ -10,16 +10,30 @@ import SwiftUI
 
 struct ContentView: View {
     
+    var colors: [Color] = [.black, .red, .green, .blue]
+    var colorNames = ["Black, Red, Green", "Blue"]
+    
+    @State private var colorIndex = 0
     @State private var rotation: Double = 0
+    @State private var text: String = "Welcome to SwiftUI"
     
     var body: some View {
         VStack {
-            VStack {
-                Text("Hello, World!")
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                Slider(value: $rotation, in: 0 ... 360, step: 0.1)
-
+        
+            Text(text)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .rotationEffect(.degrees(self.rotation))
+                .animation(.easeInOut(duration: 5))
+                .foregroundColor(self.colors[self.colorIndex])
+            Slider(value: $rotation, in: 0 ... 360, step: 0.1)
+            
+            TextField("Enter text here", text: $text).textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Picker(selection: $colorIndex, label: Text("Color")) {
+                ForEach (0 ..< colorNames.count) {
+                    Text(self.colorNames[$0]).foregroundColor(self.colors[$0])
+                }
             }
         }
     }
