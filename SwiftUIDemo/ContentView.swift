c8372a01faebb5358941f1b9400434f68b3d11a8
junhyuk <dejavukk1@gmail.com>
2020-05-13 22:34:55 +0900

    Padding, Spacer, Divider, ForEach등 다양한 뷰 레이아웃 구현.

--- SwiftUIDemo/ContentView.swift
+++ SwiftUIDemo/ContentView.swift
@@ -19,22 +19,27 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-        
+            Spacer()
             Text(text)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .rotationEffect(.degrees(self.rotation))
                 .animation(.easeInOut(duration: 5))
                 .foregroundColor(self.colors[self.colorIndex])
+            Spacer()
+            Divider()
             Slider(value: $rotation, in: 0 ... 360, step: 0.1)
+                .padding()
             
             TextField("Enter text here", text: $text).textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
             
             Picker(selection: $colorIndex, label: Text("Color")) {
                 ForEach (0 ..< colorNames.count) {
                     Text(self.colorNames[$0]).foregroundColor(self.colors[$0])
                 }
             }
+        .padding()
         }
     }
 }
